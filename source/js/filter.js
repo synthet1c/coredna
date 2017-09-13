3bf11cb55f1fc1071e334ab288f67db253258fa4
synthet1c <founts24@gmail.com>
2017-09-14 00:39:28 +1000

    cleaned up unused code

--- source/js/filter.js
+++ source/js/filter.js
@@ -1,5 +1,7 @@
 import { section, div, h3, p } from './coredna/helpers/html'
 
+const TAB = 9;
+
 const posts = [
   { id: 111, user: 'andrew', status: 'done', comment: 'sup' },
   { id: 112, user: 'sharjeel', status: 'done', comment: 'yea boi' },
@@ -15,29 +17,11 @@ const filter = document.querySelector('#filter')
 const regexify = x =>
   new RegExp('.*' + x.split('').join('.*') + '.*', 'i')
 
-/*
-const filterItems = items => e => {
-  const { value } = e.target
-  const reg = regexify(value)
-  console.log(
-    items.filter(o => reg.test(o.user))
-  )
-}
-*/
 const last = x => x[x.length - 1]
-
-const onKey = keyCode => cb => e =>
-  (e.keyCode === keyCode)
-    ? cb(e)
-    : true
-
-// filter.addEventListener('keydown', onKey(13)(filterItems(posts)), false)
 
 const underlay = document.querySelector('#underlay')
 const input = document.querySelector('#input')
 const output = document.querySelector('#output')
-
-const keys = Object.keys(posts[0])
 
 const lookup = posts.reduce((acc, obj) => {
   const keys = Object.keys(obj)
@@ -48,13 +32,7 @@ const lookup = posts.reduce((acc, obj) => {
   return acc
 }, {});
 
-const lastword = str => str.slice(str.lastIndexOf(' ') + 1);
-const lastChar = str => str[str.length - 1]
-
 console.log({ lookup })
-
-const TAB = 9;
-
 
 function placeCaretAtEnd(el) {
   el.focus();
@@ -74,43 +52,6 @@ function placeCaretAtEnd(el) {
   }
 }
 
-
-const omniboxTab = e => {
-
-  if (e.keyCode !== TAB) return true
-  e.preventDefault();
-
-  const text = e.target.innerText;
-  const last = text.split(/\s/).pop();
-  const [ key, value ] = last.split(':');
-  const regKey = new RegExp(`^${key}.*`);
-  const regValue = new RegExp(`^${value}.*`);
-  const [ match ] = keys.filter(x => regKey.test(x));
-
-  if (key && match) {
-    if (text === match) {
-      underlay.innerText = text
-      input.innerText = text
-    }
-    else {
-      let suggestion = lookup[match].find(o => regValue.test(o));
-      if (match && value && suggestion) {
-        underlay.innerText = text.slice(0, text.lastIndexOf(':')) + ':' + suggestion
-        input.innerText = text.slice(0, text.lastIndexOf(':')) + ':' + suggestion
-      }
-      else {
-        underlay.innerText = text
-        input.innerText = text
-      }
-    }
-  }
-  else {
-    underlay.innerText = text
-    input.innerText = text
-  }
-  placeCaretAtEnd(input)
-};
-
 const pairsToObject = pairs => pairs.reduce((acc, [key, value]) => {
   if (acc[key] == null) {
     acc[key] = []
@@ -119,8 +60,6 @@ const pairsToObject = pairs => pairs.reduce((acc, [key, value]) => {
   return acc
 } ,{})
 
-
-
 const filterOmnibox = fn => e => {
   const text = e.target.innerText;
   const pairs = text.split(/,*\s/).map(x => x.split(':'));
@@ -128,7 +67,6 @@ const filterOmnibox = fn => e => {
   return fn({ text, pairs, params })
 }
 
-const unary = fn => x => fn(x)
 const reg = x => new RegExp(`^${x}`)
 
 const autocompleteKey = (suggestion = '') => {
@@ -195,11 +133,11 @@ const filterItems = items => ({ text, pairs, params }) => {
   output.innerHTML = ''
 
   results.map(obj =>
-    section('.post',
+    section('.post' + '.' + obj.status,
       div('.post__inner',
         div('.post__content',
           h3('.post__heading', obj.user),
-          p('.post__copy' + '.' + obj.status, obj.comment || 'no comment')
+          p('.post__copy', obj.comment || 'no comment')
         )
       )
     )
@@ -214,42 +152,6 @@ const onTab = e => {
   input.innerText = underlay.innerText
   placeCaretAtEnd(input)
 }
-
-const omniboxKeyup = e => {
-
-  const text = e.target.innerText;
-  const last = text.split(/\s/).pop();
-  const [ key, value ] = last.split(':');
-  const regKey = new RegExp(`^${key}.*`);
-  const regValue = new RegExp(`^${value}.*`);
-  const [ match ] = keys.filter(x => regKey.test(x));
-
-  if (key && match) {
-    if (text === match) {
-      underlay.innerText = text
-    }
-    else {
-      let suggestion = lookup[match].find(o => regValue.test(o));
-      if (match && value && suggestion) {
-        underlay.innerText = text.slice(0, text.lastIndexOf(':')) + ':' + suggestion
-      }
-      else {
-        if (lastChar(text) === ':') {
-          underlay.innerText = text.slice(0, text.length - key.length - 1) + match + ':'
-        } else if (match && value && !suggestion) {
-          underlay.innerText = text
-        } else if (match && !suggestion) {
-          underlay.innerText = text.slice(0, text.length - key.length) + match
-        } else {
-          underlay.innerText = text
-        }
-      }
-    }
-  }
-  else {
-    underlay.innerText = text
-  }
-};
 
 input.addEventListener('keyup', filterOmnibox(filterItems(posts)), false);
 input.addEventListener('keydown', onTab, false);
